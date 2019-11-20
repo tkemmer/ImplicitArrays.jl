7dbe498183c08ba6dd62e53338901e533c6d1af2
Thomas Kemmer <thomas@binaryden.de>
2019-11-20 22:12:55 +0100

    Add vararg ctor and setindex! support to RowProjectionMatrix

--- src/rowprojection.jl
+++ src/rowprojection.jl
@@ -4,9 +4,28 @@ struct RowProjectionMatrix{T} <: AbstractArray{T, 2}
     rows::Vector{Int}
 end
 
-@inline Base.size(M::RowProjectionMatrix{T}) where T = (length(M.rows), size(M.base)[2])
+RowProjectionMatrix(
+    base::AbstractArray{T, 2},
+    rows::Vararg{Int, N}
+) where {T, N} = RowProjectionMatrix(base, collect(rows))
 
-@inline function Base.getindex(M::RowProjectionMatrix{T}, I::Vararg{Int, 2}) where T
-    @boundscheck checkbounds(A, I)
+@inline Base.size(
+    M::RowProjectionMatrix{T}
+) where T = (length(M.rows), size(M.base)[2])
+
+@inline function Base.getindex(
+    M::RowProjectionMatrix{T},
+    I::Vararg{Int, 2}
+) where T
+    @boundscheck checkbounds(M, I...)
     M.base[M.rows[I[1]], I[2]]
+end
+
+@inline function Base.setindex!(
+    M::RowProjectionMatrix{T},
+    v::Any,
+    I::Vararg{Int, 2}
+) where T
+    @boundscheck checkbounds(M, I...)
+    M.base[M.rows[I[1]], I[2]] = v
 end
