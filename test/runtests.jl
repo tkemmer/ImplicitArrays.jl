7c23b49e2a14c06d0fd44d1eb3990ee7d085349c
Thomas Kemmer <thomas@binaryden.de>
2020-01-23 09:08:01 +0100

    Move RowProjectionMatrix tests to correct testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,8 @@ using Test
 @testset "ImplicitArrays.jl" begin
     @testset "FixedValueArray" begin
         include("fixedvalue.jl")
+    end
+    @testset "RowProjectionMatrix" begin
         include("rowprojection.jl")
     end
 end
