9abc010e8d48ff26bcccd0a0da4e83b871a3253f
Thomas Kemmer <thomas@binaryden.de>
2020-01-27 15:04:41 +0100

    Add tests for BlockMatrix

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using ImplicitArrays
 using Test
 
 @testset "ImplicitArrays.jl" begin
+    @testset "BlockMatrix" begin include("block.jl") end
     @testset "InteractionMatrix" begin include("interaction.jl") end
     @testset "FixedValueArray" begin include("fixedvalue.jl") end
     @testset "RowProjectionMatrix" begin include("rowprojection.jl") end
