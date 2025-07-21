01d71b5b47680a7079fa97737572cc170926a710
Thomas Kemmer <thomas@binaryden.de>
2025-07-21 20:20:24 +0200

    InteractionMatrix: add tests for custom interaction functions

--- test/interaction.jl
+++ test/interaction.jl
@@ -1,6 +1,17 @@
 @testitem "InteractionMatrix" begin
     include("compat.jl")
 
+    struct SimpleFun <: InteractionFunction{Int, Float64, Float64} end
+    (::SimpleFun)(r::Int, c::Float64) = r * r + c
+
+    struct GenericFun{T <: AbstractFloat} <: InteractionFunction{Int, T, T} end
+    (::GenericFun{T})(r::Int, c::T) where T = r * r + c
+
+    struct StatefulFun{T <: AbstractFloat} <: InteractionFunction{Int, T, T}
+        α::T
+    end
+    (f::StatefulFun{T})(r::Int, c::T) where T = f.α * (r * r + c)
+
     @testset "empty matrix" begin
         A = InteractionMatrix(Int[], Int[], 11)
         @test A isa AbstractArray{Int, 2}
@@ -15,6 +26,24 @@
         @test_throws BoundsError A[1]
 
         A = InteractionMatrix{Float64}(Int[], Int[], /)
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (0, 0)
+        @test_throws BoundsError A[]
+        @test_throws BoundsError A[1]
+
+        A = InteractionMatrix(Int[], Float64[], SimpleFun())
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (0, 0)
+        @test_throws BoundsError A[]
+        @test_throws BoundsError A[1]
+
+        A = InteractionMatrix(Int[], Float64[], GenericFun{Float64}())
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (0, 0)
+        @test_throws BoundsError A[]
+        @test_throws BoundsError A[1]
+
+        A = InteractionMatrix(Int[], Float64[], StatefulFun{Float64}(2.0))
         @test A isa AbstractArray{Float64, 2}
         @test size(A) == (0, 0)
         @test_throws BoundsError A[]
@@ -39,6 +68,24 @@
         @test size(A) == (0, 3)
         @test_throws BoundsError A[]
         @test_throws BoundsError A[1]
+
+        A = InteractionMatrix(Int[], Float64[42, 40, 47], SimpleFun())
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (0, 3)
+        @test_throws BoundsError A[]
+        @test_throws BoundsError A[1]
+
+        A = InteractionMatrix(Int[], Float64[42, 40, 47], GenericFun{Float64}())
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (0, 3)
+        @test_throws BoundsError A[]
+        @test_throws BoundsError A[1]
+
+        A = InteractionMatrix(Int[], Float64[42, 40, 47], StatefulFun{Float64}(2.0))
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (0, 3)
+        @test_throws BoundsError A[]
+        @test_throws BoundsError A[1]
     end
 
     @testset "no colelems" begin
@@ -55,6 +102,24 @@
         @test_throws BoundsError A[1]
 
         A = InteractionMatrix{Float64}(Int[42, 40, 47], Int[], /)
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (3, 0)
+        @test_throws BoundsError A[]
+        @test_throws BoundsError A[1]
+
+        A = InteractionMatrix(Int[42, 40, 47], Float64[], SimpleFun())
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (3, 0)
+        @test_throws BoundsError A[]
+        @test_throws BoundsError A[1]
+
+        A = InteractionMatrix(Int[42, 40, 47], Float64[], GenericFun{Float64}())
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (3, 0)
+        @test_throws BoundsError A[]
+        @test_throws BoundsError A[1]
+
+        A = InteractionMatrix(Int[42, 40, 47], Float64[], StatefulFun{Float64}(2.0))
         @test A isa AbstractArray{Float64, 2}
         @test size(A) == (3, 0)
         @test_throws BoundsError A[]
@@ -106,6 +171,57 @@
         @test A isa AbstractArray{Float64, 2}
         @test A[1] isa Float64
         @test A[1] == 21.0
+
+        A = InteractionMatrix(Int[2], Float64[42], SimpleFun())
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (1, 1)
+        @test A[] isa Float64
+        @test A[] == 46
+        @test A[1] isa Float64
+        @test A[1] == 46
+        @test A[1, 1] isa Float64
+        @test A[1, 1] == 46
+        @test_throws BoundsError A[2]
+        @test_throws BoundsError A[1, 2]
+        @test_throws BoundsError A[2, 1]
+        @test_throws BoundsError A[2, 2]
+        @test_throws CanonicalIndexError A[] = 13
+        @test_throws CanonicalIndexError A[1] = 13
+        @test_throws CanonicalIndexError A[1, 1] = 13
+
+        A = InteractionMatrix(Int[2], Float64[42], GenericFun{Float64}())
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (1, 1)
+        @test A[] isa Float64
+        @test A[] == 46
+        @test A[1] isa Float64
+        @test A[1] == 46
+        @test A[1, 1] isa Float64
+        @test A[1, 1] == 46
+        @test_throws BoundsError A[2]
+        @test_throws BoundsError A[1, 2]
+        @test_throws BoundsError A[2, 1]
+        @test_throws BoundsError A[2, 2]
+        @test_throws CanonicalIndexError A[] = 13
+        @test_throws CanonicalIndexError A[1] = 13
+        @test_throws CanonicalIndexError A[1, 1] = 13
+
+        A = InteractionMatrix(Int[2], Float64[42], StatefulFun{Float64}(2.0))
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (1, 1)
+        @test A[] isa Float64
+        @test A[] == 92
+        @test A[1] isa Float64
+        @test A[1] == 92
+        @test A[1, 1] isa Float64
+        @test A[1, 1] == 92
+        @test_throws BoundsError A[2]
+        @test_throws BoundsError A[1, 2]
+        @test_throws BoundsError A[2, 1]
+        @test_throws BoundsError A[2, 2]
+        @test_throws CanonicalIndexError A[] = 13
+        @test_throws CanonicalIndexError A[1] = 13
+        @test_throws CanonicalIndexError A[1, 1] = 13
     end
 
     @testset "single rowelem" begin
@@ -170,6 +286,66 @@
         @test A[1, 2] == 20.0
         @test A[1, 3] isa Float64
         @test A[1, 3] == 23.5
+
+        A = InteractionMatrix(Int[2], Float64[42, 40, 47], SimpleFun())
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (1, 3)
+        @test_throws BoundsError A[]
+        @test A[1] isa Float64
+        @test A[1] == 46
+        @test A[2] isa Float64
+        @test A[2] == 44
+        @test A[3] isa Float64
+        @test A[3] == 51
+        @test A[1, 1] isa Float64
+        @test A[1, 1] == 46
+        @test A[1, 2] isa Float64
+        @test A[1, 2] == 44
+        @test A[1, 3] isa Float64
+        @test A[1, 3] == 51
+        @test_throws BoundsError A[4]
+        @test_throws BoundsError A[1, 4]
+        @test_throws BoundsError A[2, 1]
+
+        A = InteractionMatrix(Int[2], Float64[42, 40, 47], GenericFun{Float64}())
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (1, 3)
+        @test_throws BoundsError A[]
+        @test A[1] isa Float64
+        @test A[1] == 46
+        @test A[2] isa Float64
+        @test A[2] == 44
+        @test A[3] isa Float64
+        @test A[3] == 51
+        @test A[1, 1] isa Float64
+        @test A[1, 1] == 46
+        @test A[1, 2] isa Float64
+        @test A[1, 2] == 44
+        @test A[1, 3] isa Float64
+        @test A[1, 3] == 51
+        @test_throws BoundsError A[4]
+        @test_throws BoundsError A[1, 4]
+        @test_throws BoundsError A[2, 1]
+
+        A = InteractionMatrix(Int[2], Float64[42, 40, 47], StatefulFun{Float64}(2.0))
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (1, 3)
+        @test_throws BoundsError A[]
+        @test A[1] isa Float64
+        @test A[1] == 92
+        @test A[2] isa Float64
+        @test A[2] == 88
+        @test A[3] isa Float64
+        @test A[3] == 102
+        @test A[1, 1] isa Float64
+        @test A[1, 1] == 92
+        @test A[1, 2] isa Float64
+        @test A[1, 2] == 88
+        @test A[1, 3] isa Float64
+        @test A[1, 3] == 102
+        @test_throws BoundsError A[4]
+        @test_throws BoundsError A[1, 4]
+        @test_throws BoundsError A[2, 1]
     end
 
     @testset "single colelem" begin
@@ -234,6 +410,66 @@
         @test A[2, 1] == 20.0
         @test A[3, 1] isa Float64
         @test A[3, 1] == 23.5
+
+        A = InteractionMatrix(Int[42, 40, 47], Float64[2], SimpleFun())
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (3, 1)
+        @test_throws BoundsError A[]
+        @test A[1] isa Float64
+        @test A[1] == 1766
+        @test A[2] isa Float64
+        @test A[2] == 1602
+        @test A[3] isa Float64
+        @test A[3] == 2211
+        @test A[1, 1] isa Float64
+        @test A[1, 1] == 1766
+        @test A[2, 1] isa Float64
+        @test A[2, 1] == 1602
+        @test A[3, 1] isa Float64
+        @test A[3, 1] == 2211
+        @test_throws BoundsError A[4]
+        @test_throws BoundsError A[4, 1]
+        @test_throws BoundsError A[1, 2]
+
+        A = InteractionMatrix(Int[42, 40, 47], Float64[2], GenericFun{Float64}())
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (3, 1)
+        @test_throws BoundsError A[]
+        @test A[1] isa Float64
+        @test A[1] == 1766
+        @test A[2] isa Float64
+        @test A[2] == 1602
+        @test A[3] isa Float64
+        @test A[3] == 2211
+        @test A[1, 1] isa Float64
+        @test A[1, 1] == 1766
+        @test A[2, 1] isa Float64
+        @test A[2, 1] == 1602
+        @test A[3, 1] isa Float64
+        @test A[3, 1] == 2211
+        @test_throws BoundsError A[4]
+        @test_throws BoundsError A[4, 1]
+        @test_throws BoundsError A[1, 2]
+
+        A = InteractionMatrix(Int[42, 40, 47], Float64[2], StatefulFun{Float64}(2.0))
+        @test A isa AbstractArray{Float64, 2}
+        @test size(A) == (3, 1)
+        @test_throws BoundsError A[]
+        @test A[1] isa Float64
+        @test A[1] == 1766 * 2
+        @test A[2] isa Float64
+        @test A[2] == 1602 * 2
+        @test A[3] isa Float64
+        @test A[3] == 2211 * 2
+        @test A[1, 1] isa Float64
+        @test A[1, 1] == 1766 * 2
+        @test A[2, 1] isa Float64
+        @test A[2, 1] == 1602 * 2
+        @test A[3, 1] isa Float64
+        @test A[3, 1] == 2211 * 2
+        @test_throws BoundsError A[4]
+        @test_throws BoundsError A[4, 1]
+        @test_throws BoundsError A[1, 2]
     end
 
     @testset "ordinary" begin
@@ -345,5 +581,56 @@
         @test A[1, 2] == 8.4
         @test A[2, 2] isa Float64
         @test A[2, 2] == 8.0
+
+        A = InteractionMatrix(Int[1, 2, 3], Float64[10, 20, 30], SimpleFun())
+        @test A isa AbstractMatrix{Float64}
+        @test eltype(A) == Float64
+        @test size(A) == (3, 3)
+        @test A == Float64[11 21 31; 14 24 34; 19 29 39]
+        @test_throws BoundsError A[]
+        @test A[5] isa Float64
+        @test A[5] == 24
+        @test A[2, 2] isa Float64
+        @test A[2, 2] == 24
+        @test_throws BoundsError A[10]
+        @test_throws BoundsError A[4, 1]
+        @test_throws BoundsError A[1, 4]
+        @test_throws BoundsError A[] = 13
+        @test_throws CanonicalIndexError A[5] = 13
+        @test_throws CanonicalIndexError A[2, 2] = 13
+
+        A = InteractionMatrix(Int[1, 2, 3], Float64[10, 20, 30], GenericFun{Float64}())
+        @test A isa AbstractMatrix{Float64}
+        @test eltype(A) == Float64
+        @test size(A) == (3, 3)
+        @test A == Float64[11 21 31; 14 24 34; 19 29 39]
+        @test_throws BoundsError A[]
+        @test A[5] isa Float64
+        @test A[5] == 24
+        @test A[2, 2] isa Float64
+        @test A[2, 2] == 24
+        @test_throws BoundsError A[10]
+        @test_throws BoundsError A[4, 1]
+        @test_throws BoundsError A[1, 4]
+        @test_throws BoundsError A[] = 13
+        @test_throws CanonicalIndexError A[5] = 13
+        @test_throws CanonicalIndexError A[2, 2] = 13
+
+        A = InteractionMatrix(Int[1, 2, 3], Float64[10, 20, 30], StatefulFun{Float64}(2.0))
+        @test A isa AbstractMatrix{Float64}
+        @test eltype(A) == Float64
+        @test size(A) == (3, 3)
+        @test A == 2 .* Float64[11 21 31; 14 24 34; 19 29 39]
+        @test_throws BoundsError A[]
+        @test A[5] isa Float64
+        @test A[5] == 48
+        @test A[2, 2] isa Float64
+        @test A[2, 2] == 48
+        @test_throws BoundsError A[10]
+        @test_throws BoundsError A[4, 1]
+        @test_throws BoundsError A[1, 4]
+        @test_throws BoundsError A[] = 13
+        @test_throws CanonicalIndexError A[5] = 13
+        @test_throws CanonicalIndexError A[2, 2] = 13
     end
 end
